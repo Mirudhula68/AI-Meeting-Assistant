ac8c8c3e1a59beba4dcd66675ea0fd3b992dd75b
Mirudhula <mirudhuramu06@gmail.com>
2026-04-23 14:29:22 +0530

    Final Commit

--- ml-service/app/services/speech_to_text.py
+++ ml-service/app/services/speech_to_text.py
@@ -1,21 +1,7 @@
-import os
-from deepgram import DeepgramClient
+import whisper
 
-# Get API key from environment
-api_key = os.getenv("DEEPGRAM_API_KEY")
-
-if not api_key:
-    raise ValueError("Deepgram API key missing!")
-
-# Correct initialization (NO direct parameter)
-deepgram = DeepgramClient()
+model = whisper.load_model("base")
 
 def transcribe_audio(file_path):
-
-    with open(file_path, "rb") as audio:
-        response = deepgram.listen.prerecorded.v("1").transcribe_file(
-            {"buffer": audio.read()},
-            {"punctuate": True}
-        )
-
-    return response["results"]["channels"][0]["alternatives"][0]["transcript"]
+    result = model.transcribe(file_path)
+    return result["text"]
